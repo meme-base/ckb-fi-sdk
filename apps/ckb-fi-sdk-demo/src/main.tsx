e734ff1da37888985fbc9f658a765f12791e335a
qinhua <352484005@qq.com>
2025-01-10 01:37:17 +0800

    feat: sdk init

--- apps/ckb-fi-sdk-demo/src/main.tsx
+++ apps/ckb-fi-sdk-demo/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "react-toastify/dist/ReactToastify.css";
 import "./assets/style/index.scss";
-import App from "./App.tsx";
+import App from "./App";
 // import "virtual:windi.css";
 
 const toastRoot = document.createElement("div");
